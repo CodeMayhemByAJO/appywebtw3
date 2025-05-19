d934c1065c7285d10acffe9d2b64b0630d5aa972
andreas <andreas.jonsson22@edu.edugrade.se>
2025-05-19 13:55:03 +0200

    Allt funkar!!!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './**/*.html'],
+  safelist: ['hover:text-[#38776e]'],
   theme: {
     extend: {
       colors: {
-        appyYellow: '#F7A42B', // Ersätt med din faktiska guldfärg
-        appyOrange: '#F1531A', // Ersätt med din faktiska orangefärg
-        appyTurquoise: '#2BAAA8', // Ersätt med din faktiska turkosfärg
-        appyNavy: '#1B1E3C', // Ersätt med din faktiska marinblå färg
-        appyBeige: '#FAEAD7', // Ersätt med din faktiska beigefärg
+        appyYellow: '#F7A42B',
+        appyOrange: '#F1531A',
+        appyTurquoise: '#2BAAA8',
+        appyNavy: '#1B1E3C',
+        appyBeige: '#FAEAD7',
       },
       fontFamily: {
         edu: ['"Edu VIC WA NT Beginner"', 'cursive'],
